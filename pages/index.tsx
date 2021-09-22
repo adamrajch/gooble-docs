dec43e92d04e8d9a2e521bb46e239d8902a5f8d6
adamrajch <arajchwald@gmail.com>
2021-09-22 15:54:59 -0500

    center

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {loading && (
-        <div className=" flex justify-center items-center">
+        <div className=" flex justify-center items-center min-h-screen">
           <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
         </div>
       )}
--- pages/login.tsx
+++ pages/login.tsx
@@ -35,7 +35,7 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         {loading && (
-          <div className=" flex justify-center items-center">
+          <div className="flex justify-center items-center h-full">
             <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
           </div>
         )}
